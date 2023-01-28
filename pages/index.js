57a9df4c90a61c6feb4c8dd7318b306b3251970e
pawitpr <123424956+pawitpr@users.noreply.github.com>
2023-01-29 02:42:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,16 @@ import SearchForm from '../lib/SearchForm';
 export default function Home(props) {
   return (
     <main>
-      <h1>Create a Car</h1>
-      <CarForm />
-      <hr />
       <h1>Find a Car</h1>
       <SearchForm />
     </main>
   );
+  export const config = {
+  runtime: 'edge', // for Edge API Routes only
+  unstable_allowDynamic: [
+    '/lib/utilities.js', // allows a single file
+    '/node_modules/function-bind/**', // use a glob to allow anything in the function-bind 3rd party module
+  ],
+}
+
 }
